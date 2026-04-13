42e70f8e634c9414d20e78c7087d5f3414520a38
Nitish Bhardwaj <Nitishbhardwaj1111@gmail.com>
2026-04-13 11:53:39 +0530

    updated env file and its dependencies for deployment configuration.

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
+        host=os.getenv("PG_HOST"),
+        port=int(os.getenv("PG_PORT", 3306)),
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD"),
+        database=os.getenv("PG_DB"),
     )
 
 
